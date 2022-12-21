247df8ade3f774024588cf01aea11e50cc36722e
oscarboe <Oscarboen@gmail.com>
2022-12-21 12:59:31 +0100

    mongoclient should load cards now

--- Heathstone/Heathstone/Services/MongoService.cs
+++ Heathstone/Heathstone/Services/MongoService.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Driver;
 using System.Text.Json;
+using Heathstone.Models;
 
 namespace MTGCollection.Services;
 
@@ -9,12 +10,12 @@ public class MongoService
     private readonly MongoClient _client;
     public MongoService()
     {
-        _client = new MongoClient("mongodb://root:example@mongo:27017");
+        _client = new MongoClient("mongodb://localhost:5000");
         var db = _client.GetDatabase("mtg");
         if (_client.GetDatabase("mtg").ListCollections().ToList().Count == 0)
         {
             var collection = db.GetCollection<Card>("cards");
-            foreach (var path in new[] { "lea.json", "arn.json", "atq.json", "leg.json" })
+            foreach (var path in new[] { "cards.json" })
             {
                 using (var file = new StreamReader(path))
                 {
